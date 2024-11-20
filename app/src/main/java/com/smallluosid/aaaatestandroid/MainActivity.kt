19a39cd0b4ff310bf9886f62f8c103f7049f6498
zhaojian <18310579837@163.com>
2024-11-20 15:42:29 +0800

    检查网络权限

--- app/src/main/java/com/smallluosid/aaaatestandroid/MainActivity.kt
+++ app/src/main/java/com/smallluosid/aaaatestandroid/MainActivity.kt
@@ -4,11 +4,16 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.core.content.ContextCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<View>(R.id.checkInternet).setOnClickListener {
+            val check = ContextCompat.checkSelfPermission(this@MainActivity, android.Manifest.permission.INTERNET)
+            Toast.makeText(this@MainActivity, "是否有网络权限 : ${check}", Toast.LENGTH_SHORT).show()
+        }
         findViewById<View>(R.id.playMusic).setOnClickListener {
             BgmHelper.playBgm()
         }
